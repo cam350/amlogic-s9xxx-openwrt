4009b1d6f6eaa00d8fe4d624fb506a1097ca1ce9
cam350 <jingjing1234567@163.com>
2026-07-11 10:19:38 +0800

    docker + openclaw

--- config/lede_master/diy-part1.sh
+++ config/lede_master/diy-part1.sh
@@ -8,6 +8,11 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+sed -i '3i src-git openclaw https://github.com/10000ge10000/luci-app-openclaw.git' feeds.conf.default
+
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
